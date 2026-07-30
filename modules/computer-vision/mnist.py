8aaf25221676c5c6d5e04deff67f2c88e7d05eae
matthew-pisano <matthewpisano14@gmail.com>
2026-07-30 08:15:47 -0400

    Document CNN and DNN

--- modules/computer-vision/mnist.py
+++ modules/computer-vision/mnist.py
@@ -20,18 +20,26 @@ class ConvNet(nn.Module):
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.linear1 = nn.Linear(9216, 128)
+        self.linear2 = nn.Linear(128, 10)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # Convolutional layers extract underlying patterns (edges, shapes) from the raw image input
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
+        # Max pooling reduces the spatial dimensions of the feature maps, retaining the most important information
         x = F.max_pool2d(x, 2)
+        # Dropout to selectively deactivate neurons during training to avoid overfitting, lighter after conv layers
         x = self.dropout1(x)
+        # Flatten the dimensions from the CNN into a 1D tensor that the linear layers can consume
         x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
+        # Linear layer for processing CNN features
+        x = F.relu(self.linear1(x))
+        # Dropout to selectively deactivate neurons during training to avoid overfitting, heavier after fc layer
         x = self.dropout2(x)
-        x = self.fc2(x)
+        # Second linear layer for further processing
+        x = self.linear2(x)
+        # Log softmax to convert the output into log probabilities for each class (log useful during loss evaluation)
         return F.log_softmax(x, dim=1)
 
 
